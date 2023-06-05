b249d68f9c36e217b915b553326e230b1c293d93
AzizbekGulomov2002 <azizgulomov1529@gmail.com>
2023-06-05 10:11:21 +0300

    deploy-check-ielts-new-version

--- project/settings.py
+++ project/settings.py
@@ -118,12 +118,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static/']
-MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "media/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
