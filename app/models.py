a44786300e93113780056052e8391501b74f89f5
AzizbekGulomov2002 <azizgulomov2529@gmail.com>
2023-12-06 18:57:17 +0500

    deploy

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from asyncio import format_helpers
-import math 
+import math
 sex = [
     ('F','Female'),
     ('M','Male'),
@@ -8,31 +8,30 @@ sex = [
 
 
 class IELTS(models.Model):
-    image = models.ImageField(upload_to='image_candidat', null=True, blank=True)
+    image = models.ImageField(upload_to='image_candidate', null=True, blank=True)
     candidate_id = models.CharField(max_length=9)
     date = models.DateField()
     family_name = models.CharField(max_length=200)
     first_name = models.CharField(max_length=200)
-    
+
     date_of_birth = models.DateField(help_text='Year | Month | Day')
     sex = models.CharField(choices=sex, max_length=2)
     region_address = models.CharField(max_length=200)
     nationally = models.CharField(max_length=100)
     first_language = models.CharField(max_length=100)
-    
     listening = models.FloatField(null=True, blank=True)
-    speaking = models.FloatField(null=True, blank=True)
     reading = models.FloatField(null=True, blank=True)
     writing = models.FloatField(null=True, blank=True)
-    
-    
+    speaking = models.FloatField(null=True, blank=True)
+    # overal = models.FloatField()
+    # cefr_level = models.CharField(max_length=100)
     admission_comments = models.TextField(null=True, blank=True)
     test_number = models.IntegerField()
     candidate_number = models.IntegerField(null=True, blank=True, unique=True)
 
     @property
     def overal(self):
-        arifmethic_mean = (self.listening + self.speaking + self.reading + self.writing) / 4
+        arifmethic_mean = (self.listening + self.reading + self.writing + self.speaking) / 4
         if arifmethic_mean != int(arifmethic_mean):
             floor_plus_half = int(arifmethic_mean) + 0.5
             if (arifmethic_mean) <= floor_plus_half:
@@ -43,7 +42,7 @@ class IELTS(models.Model):
             return arifmethic_mean
 
 
-  
+
 
     @property
     def cefr_level(self):
@@ -53,7 +52,7 @@ class IELTS(models.Model):
             return "B2"
         if 7 <= (self.overal) <= 8:
             return "C1"
-        if 8 <= (self.overal) <= 9: 
+        if 8 <= (self.overal) <= 9:
             return "C2"
 
 
@@ -65,7 +64,7 @@ class IELTS(models.Model):
         if self.id:
             self.candidate_number = self.id +10000
         super(IELTS, self).save(*args, **kwargs)
-        
+
     def __str__(self):
         return f"{self.candidate_number} | {self.family_name} | {self.overal} | | {self.candidate_id}"
 
@@ -76,8 +75,8 @@ class IELTS(models.Model):
 
 
 
-    
-    
+
+
 class Pupil(models.Model):
     name = models.CharField(max_length=400, help_text="Enter Pupil name....")
     date = models.DateField(null=True, blank=True, help_text="Date | Month | Year")
@@ -96,6 +95,6 @@ class Pupil(models.Model):
         if self.id:
             self.one_id = self.id +1000
         super(Pupil, self).save(*args, **kwargs)
-        
+
     def __str__(self):
         return f"{self.name} | {self.email_address} | {self.phone_number} | {self.payment} | {self.one_id}"
