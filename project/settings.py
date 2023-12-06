a4617a970be3cdb2c2286dfa748246e63ed63194
AzizbekGulomov2002 <azizgulomov2529@gmail.com>
2023-12-06 19:43:14 +0500

    deploy

--- project/settings.py
+++ project/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 import os
 
-# STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'static'
