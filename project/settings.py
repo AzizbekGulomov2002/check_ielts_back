9b8cf0d1c8ee8099acc553c7f14d0bc22b74ccf5
AzizbekGulomov2002 <azizgulomov2529@gmail.com>
2023-12-06 19:45:21 +0500

    deploy

--- project/settings.py
+++ project/settings.py
@@ -121,13 +121,10 @@ USE_TZ = True
 import os
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static'
-# ]
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static/']
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
